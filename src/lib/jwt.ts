1b30f246a141df61b3a75032a6eeb83c8c9b9393
Bit-Barron <azerabdu5@gmail.com>
2024-07-11 21:56:03 +0200

    asd

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,30 +1,34 @@
-import { serverEnv } from "../utils/env/server";
-import { createCipheriv, createDecipheriv, pbkdf2Sync } from "crypto";
+import { serverEnv } from "~/utils/env/server";
+import * as jose from "jose";
 
-const derivedKey = pbkdf2Sync(serverEnv.SECRET, "salt", 1000, 32, "sha256");
+const key = new TextEncoder().encode(serverEnv.SECRET);
 
-export const encrypt = (value: any): string | null => {
+export const encrypt = async (value: any): Promise<string | null> => {
   try {
     const text = typeof value === "object" ? JSON.stringify(value) : value;
-    const cipher = createCipheriv("aes-256-ecb", derivedKey, null);
-    let encrypted = cipher.update(text, "utf8", "base64");
-    encrypted += cipher.final("base64");
-    return encrypted;
+
+    const jwe = await new jose.CompactEncrypt(new TextEncoder().encode(text))
+      .setProtectedHeader({ alg: "dir", enc: "A256GCM" })
+      .encrypt(key);
+
+    return jwe;
   } catch (error) {
     return null;
   }
 };
 
-export const decrypt = <T = string>(encryptedText: string): T | null => {
+export const decrypt = async <T = string>(
+  encryptedText: string
+): Promise<T | null> => {
   if (typeof encryptedText !== "string") return null;
   try {
-    const decipher = createDecipheriv("aes-256-ecb", derivedKey, null);
-    let decrypted = decipher.update(encryptedText, "base64", "utf8");
-    decrypted += decipher.final("utf8");
+    const { plaintext } = await jose.compactDecrypt(encryptedText, key);
+    const decrypted = new TextDecoder().decode(plaintext);
+
     try {
       return JSON.parse(decrypted) as T;
     } catch (error) {
-      return decrypted as any;
+      return decrypted as T;
     }
   } catch (error) {
     return null;
