6c44e4df4cea0dda197207fa8af13d65411215a2
Bit-Barron <azerabdu5@gmail.com>
2024-07-18 14:25:00 +0200

    clean up

--- src/routes/auth/register/(register).tsx
+++ src/routes/auth/register/(register).tsx
@@ -1,19 +1,39 @@
-import { createSignal } from "solid-js";
+import { createSignal, JSX } from "solid-js";
 
 export default function Register() {
   const [username, setUsername] = createSignal<string>("");
   const [password, setPassword] = createSignal<string>("");
   const [status, setStatus] = createSignal<string>("");
 
+  const onSubmit: JSX.EventHandler<HTMLFormElement, SubmitEvent> = async (
+    e
+  ) => {
+    e.preventDefault();
+
+    console.log(username(), password());
+  };
+
   return (
-    <form class="space-y-2">
+    <form class="space-y-2" onSubmit={onSubmit}>
       <div class="flex flex-col border">
         <label>Username</label>
-        <input id="username" type="text" required />
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          value={username()}
+          id="username"
+          type="text"
+          required
+        />
       </div>
       <div class="flex flex-col border">
         <label>Password</label>
-        <input id="password" type="password" required />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password()}
+          id="password"
+          type="password"
+          required
+        />
       </div>
       <div class="flex justify-between">
         <button class="bg-red-600" type="submit">
